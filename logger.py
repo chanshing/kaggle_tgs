a4cd01029dbe45ea247bcd700f7b6776ed9f48a4
Michael <cshing.m@gmail.com>
2018-10-06 19:00:33 +0100

    Now requires a fixed train set in instantiation. Also removed 'tanh_mode'.

--- logger.py
+++ logger.py
@@ -11,9 +11,11 @@ import torchvision.utils as vutils
 import utils
 
 class Logger(object):
-    """ Score and masks """
-    def __init__(self, outf, netG, images_test, masks_test, tanh_mode):
+    """ Scores and masks """
+    def __init__(self, outf, netG, images_train, masks_train, images_test, masks_test):
         self.netG = netG
+        self.images_train = images_train
+        self.masks_train = masks_train
         self.images_test = images_test
         self.masks_test = masks_test
         self.outf = outf
@@ -21,31 +23,38 @@ class Logger(object):
         self.scores_train = []
         self.scores_test = []
 
-        self.tanh_mode = tanh_mode
+    def flush(self, i):
+        self.eval_netG()
+        self.update_scores()
+        self.show_masks(i)
 
-        self.score_cutoff = 0. if tanh_mode else 0.5
+    def eval_netG(self):
+        self.netG.eval()
+        with torch.no_grad():
+            self.masks_train_pred = self.netG(self.images_train)
+            self.masks_test_pred = self.netG(self.images_test)
+        self.netG.train()
 
-        self.vutils_normalize = True if tanh_mode else False
-        self.vutils_range = (-1,1) if tanh_mode else None
+    def show_masks(self, i):
+        idxs_train = np.random.choice(len(self.masks_train), size=8, replace=False)
+        idxs_test = np.random.choice(len(self.masks_test), size=8, replace=False)
+        vutils.save_image(torch.cat([self.masks_train[idxs_train], self.masks_train_pred[idxs_train]]),
+                          '{}/masks_TRAIN_{}.png'.format(self.outf, i))
+        vutils.save_image(torch.cat([self.masks_test[idxs_test], self.masks_test_pred[idxs_test]]),
+                          '{}/masks_TEST{}.png'.format(self.outf, i))
 
-    def get_score(self, masks_pred, masks):
-        masks_pred = masks_pred.detach().cpu().numpy()
-        masks = masks.detach().cpu().numpy()
-        return utils.get_score(masks_pred, masks, self.score_cutoff)
-
-    def flush(self, i, masks_pred_train, masks_train):
-
-        self.scores_train.append(self.get_score(masks_pred_train, masks_train).item())
-
-        masks_pred_test = self.get_masks(self.images_test)
-        self.scores_test.append(self.get_score(masks_pred_test, self.masks_test).item())
-
-        vutils.save_image(torch.cat([self.masks_test[:8], masks_pred_test[:8]]), '{}/masks_{}.png'.format(self.outf, i), normalize=self.vutils_normalize, range=self.vutils_range)
-        vutils.save_image(torch.cat([masks_train[:8], masks_pred_train[:8]]), '{}/masks_train_{}.png'.format(self.outf, i), normalize=self.vutils_normalize, range=self.vutils_range)
+    def update_scores(self):
+        self.scores_train.append(self.get_score(self.masks_train_pred, self.masks_train))
+        self.scores_test.append(self.get_score(self.masks_test_pred, self.masks_test))
 
         self.plot_curves(self.scores_train, 'train', self.scores_test, 'test', 'score')
         np.save('{}/scores_train.npy'.format(self.outf), np.array(self.scores_train))
         np.save('{}/scores_test.npy'.format(self.outf), np.array(self.scores_test))
+
+    def get_score(self, masks_pred, masks):
+        masks_pred = masks_pred.detach().cpu().numpy()
+        masks = masks.detach().cpu().numpy()
+        return utils.get_score(masks_pred, masks)
 
     def plot_curves(self, c1, l1, c2, l2, fname):
         fig, ax = plt.subplots()
@@ -55,65 +64,55 @@ class Logger(object):
         fig.savefig('{}/{}.png'.format(self.outf, fname))
         plt.close(fig)
 
-    def get_masks(self, images):
-        self.netG.eval()
-        with torch.no_grad():
-            masks = self.netG(images)
-        self.netG.train()
-        return masks
-
 class LoggerBCE(Logger):
-    def __init__(self, outf, netG, images_test, masks_test, tanh_mode, bcefunc):
-        # super(LoggerBCE, self).__init__(self, outf, netG, images_test, masks_test, score_cutoff)
-        Logger.__init__(self, outf, netG, images_test, masks_test, tanh_mode)
-        self.losses_train = []
-        self.losses_test = []
+    """ Extend Logger to include BCE losses """
+    def __init__(self, outf, netG, images_train, masks_train, images_test, masks_test, bcefunc):
+        Logger.__init__(self, outf, netG, images_train, masks_train, images_test, masks_test)
         self.bcefunc = bcefunc
+        self.bcelosses_train = []
+        self.bcelosses_test = []
 
-    def flush(self, i, masks_pred_train, masks_train, loss_train):
-        # super(LoggerBCE, self).flush(self, i, masks_pred_train, masks_train)
-        Logger.flush(self, i, masks_pred_train, masks_train)
+    def flush(self, i):
+        Logger.flush(self, i)
+        self.update_bcelosses()
 
-        self.losses_train.append(loss_train)
-        masks_pred_test = self.get_masks(self.images_test)
-        self.losses_test.append(self.bcefunc(masks_pred_test, self.masks_test).item())
+    def update_bcelosses(self):
+        self.bcelosses_train.append(self.bcefunc(self.masks_train_pred, self.masks_train).item())
+        self.bcelosses_test.append(self.bcefunc(self.masks_test_pred, self.masks_test).item())
 
-        self.plot_curves(self.losses_train, 'train', self.losses_test, 'test', 'loss')
-        np.save('{}/losses_train.npy'.format(self.outf), np.array(self.losses_train))
-        np.save('{}/losses_test.npy'.format(self.outf), np.array(self.losses_test))
+        self.plot_curves(self.bcelosses_train, 'train', self.bcelosses_test, 'test', 'bceloss')
+        np.save('{}/bcelosses_train.npy'.format(self.outf), np.array(self.bcelosses_train))
+        np.save('{}/bcelosses_test.npy'.format(self.outf), np.array(self.bcelosses_test))
 
 class LoggerGAN(LoggerBCE):
-    def __init__(self, outf, netG, images_test, masks_test, tanh_mode, bcefunc):
-        # super(LoggerGAN, self).__init__(self, outf, netG, images_test, masks_test, score_cutoff)
-        LoggerBCE.__init__(self, outf, netG, images_test, masks_test, tanh_mode, bcefunc)
-        self.losses = []
+    """ Extend LoggerBCE to include GAN status """
+    def __init__(self, outf, netG, images_train, masks_train, images_test, masks_test, bcefunc):
+        LoggerBCE.__init__(self, outf, netG, images_train, masks_train, images_test, masks_test, bcefunc)
+        self.ipms = []
         self.alphas = []
         self.omegas = []
 
-    def dump(self, loss, alpha, omega):
-        self.losses.append(loss)
+    def dump(self, ipm, alpha, omega):  # to track status of GAN training
+        self.ipms.append(ipm)
         self.alphas.append(alpha)
         self.omegas.append(omega)
 
-    def flush(self, i, masks_pred_train, masks_train, bceloss):
-        # super(LoggerGAN, self).flush(self, i, masks_pred_train, masks_train)
-        LoggerBCE.flush(self, i, masks_pred_train, masks_train, bceloss)
-        self.plot_losses()
-        self.save_losses()
+    def flush(self, i):
+        LoggerBCE.flush(self, i)
+        self.update_ganstatus()
 
-    def save_losses(self):
-        np.save('{}/losses.npy'.format(self.outf), np.array(self.losses))
-        np.save('{}/alphas.npy'.format(self.outf), np.array(self.alphas))
-        np.save('{}/omegas.npy'.format(self.outf), np.array(self.omegas))
-
-    def plot_losses(self):
+    def update_ganstatus(self):
         fig, axs = plt.subplots(3, 1, sharex=True)
         axs[0].set_ylabel('IPM')
-        axs[0].plot(medfilt(self.losses, 101)[50:-50]); axs[0].set_yscale('symlog')
+        axs[0].plot(medfilt(self.ipms, 101)[50:-50]); axs[0].set_yscale('symlog')
         axs[1].set_ylabel(r'$\alpha$')
         axs[1].plot(medfilt(self.alphas, 101)[50:-50])
         axs[2].set_ylabel(r'$\Omega$')
         axs[2].semilogy(medfilt(self.omegas, 101)[50:-50])
         axs[2].set_xlabel('iteration')
-        fig.savefig('{}/loss.png'.format(self.outf))
+        fig.savefig('{}/status.png'.format(self.outf))
         plt.close(fig)
+
+        np.save('{}/ipms.npy'.format(self.outf), np.array(self.ipms))
+        np.save('{}/alphas.npy'.format(self.outf), np.array(self.alphas))
+        np.save('{}/omegas.npy'.format(self.outf), np.array(self.omegas))
